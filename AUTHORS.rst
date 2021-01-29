133ef1d24dd22d685510f1436e794a0ab2a704ec
Andrew Schechtman-Rook <rook166@gmail.com>
2021-01-29 09:50:55 -0500

    cleaning up setup

---
+++ AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Andrew Schechtman-Rook <football_astronomer@gmail.com>
+
+Contributors
+------------
+
+None yet. Why not be the first?
--- setup.py
+++ setup.py
@@ -8,29 +8,37 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-requirements = [ ]
+# TODO: Coordinate with environment.yml
+requirements = [
+    'dask==2021.1.0',
+    'numpy==1.19.5',
+    'pandas==1.2.0',
+    'plotly==4.14.3',
+    'svgpathtools==1.4.1'
+    'versioneer=0.19'
+]
 
-setup_requirements = ['pytest-runner', ]
+dev_packages = ['flake8==3.8.4', 'pytest==6.2.1', 'pytest-cov==2.11.1']
 
-test_requirements = ['pytest>=3', ]
+extra_requirements = {'dev': dev_packages}
 
 setup(
     author="Andrew Schechtman-Rook",
     author_email='footballastronomer@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     cmdclass=versioneer.get_cmdclass(),
     description="Make beautiful plots of player tracking data",
+    extras_require=extra_requirements,
     install_requires=requirements,
     license="GNU General Public License v3",
     long_description=readme,
@@ -38,10 +46,7 @@ setup(
     keywords='ptplot',
     name='ptplot',
     packages=find_packages(include=['ptplot', 'ptplot.*']),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
     url='https://github.com/AndrewRook/ptplot',
-    version=versioneer.get_version()
+    version=versioneer.get_version(),
     zip_safe=False,
 )
