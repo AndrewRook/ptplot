d17536f5e6f85b8ff5c6c29fd1c193c461db2e8b
Andrew Schechtman-Rook <rook166@gmail.com>
2021-02-07 08:32:05 -0500

    added some nocover flags

--- ptplot/utilities.py
+++ ptplot/utilities.py
@@ -6,7 +6,7 @@ import warnings
 from typing import Sequence, Union
 
 
-class DaskCompatibilityWarning(UserWarning):
+class DaskCompatibilityWarning(UserWarning):  # pragma: no cover
     """
     A warning for cases where some noncritical functionality
     is skipped because it does not work in dask. For example,
@@ -19,7 +19,7 @@ class DaskCompatibilityWarning(UserWarning):
     pass
 
 
-def warn_if_dask(input_dataframe):
+def warn_if_dask(input_dataframe):  # pragma: no cover
     if type(input_dataframe) == dd.DataFrame:
         warnings.warn(
             "Error checking suppressed with dask dataframes to prevent " "unnecessary compute calls",
@@ -27,19 +27,19 @@ def warn_if_dask(input_dataframe):
         )
 
 
-def get_path_midpoint(path):
+def get_path_midpoint(path):  # pragma: no cover
     min_x, max_x, min_y, max_y = path.bbox()
     midpoint_x = (max_x + min_x) / 2
     midpoint_y = (max_y + min_y) / 2
     return complex(midpoint_x, midpoint_y)
 
 
-def make_vline(y0, y1, x, **kwargs):
+def make_vline(y0, y1, x, **kwargs):  # pragma: no cover
     """Make a vertical line of a defined distance."""
     return dict(type="line", y0=y0, y1=y1, x0=x, x1=x, **kwargs)
 
 
-def make_hline(x0, x1, y, **kwargs):
+def make_hline(x0, x1, y, **kwargs):  # pragma: no cover
     """Make a horizontal line of a defined distance."""
     return dict(type="line", y0=y, y1=y, x0=x0, x1=x1, **kwargs)
 
