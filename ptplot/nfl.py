aa7ff9334cba2016878aa9918a9be41d101a94e6
Andrew Schechtman-Rook <rook166@gmail.com>
2021-07-10 14:59:23 -0400

    bumped minimum bokeh version, switched ball to ellipse

--- ptplot/nfl.py
+++ ptplot/nfl.py
@@ -60,11 +60,10 @@ NFL_TEAMS = {
 
 
 def _ball_marker_generator(figure: figure) -> Callable[[figure], Callable[..., GlyphRenderer]]:
-    return partial(figure.circle, radius=1, line_width=2, fill_alpha=0.9, fill_color="brown", line_color="brown")
-    # return partial(
-    #     figure.ellipse, width=2, height=1, angle=0.0,
-    #     fill_color="brown", fill_alpha=0.9, line_color="brown"
-    # )
+    return partial(
+        figure.ellipse, width=2, height=1, angle=0.0,
+        fill_color="brown", fill_alpha=0.9, line_color="brown"
+    )
 
 
 class Aesthetics(_Aesthetics):
