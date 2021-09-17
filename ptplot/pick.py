4a477eec2ff9c875efe0915c41db52f048e9fac8
Andrew Schechtman-Rook <rook166@gmail.com>
2021-09-17 09:01:35 -0400

    actually kind of have circle-based pick working

--- ptplot/pick.py
+++ ptplot/pick.py
@@ -1,30 +1,10 @@
-from bokeh.models.glyph import LineGlyph, FillGlyph
+from bokeh.models.glyphs import Circle
 from bokeh.core.property.dataspec import field
-from bokeh.core.properties import AngleSpec, Include, NullDistanceSpec, NumberSpec
-from bokeh.core.property_mixins import LineProps, FillProps
+from bokeh.core.properties import AngleSpec
 
 
-class Pick(LineGlyph, FillGlyph):
+class Pick(Circle):
     __implementation__ = "pick.ts"
     _args = ("x", "y", "rot")
 
-    radius = NullDistanceSpec()
-    x = NumberSpec(default=field("x"))
-    y = NumberSpec(default=field("y"))
     rot = AngleSpec(default=field("rot"))
-
-    line_props = Include(
-        LineProps,
-        use_prefix=False,
-        help="""
-    The {prop} values for the Pick.
-    """,
-    )
-
-    fill_props = Include(
-        FillProps,
-        use_prefix=False,
-        help="""
-    The {prop} values for the Pick.
-    """,
-    )
