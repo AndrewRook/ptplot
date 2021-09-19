b9db6308c6649fdc8fde9c5b506a114643a8291a
Andrew Schechtman-Rook <rook166@gmail.com>
2021-09-19 11:15:52 -0400

    trying to ignore type hints

--- ptplot/__init__.py
+++ ptplot/__init__.py
@@ -1,4 +1,5 @@
 from .ptplot import PTPlot  # noqa: F401
-# from ._version import get_versions # type: ignore
+from ._version import get_versions  # type: ignore
+
 #
 # __version__ = get_versions()["version"]
